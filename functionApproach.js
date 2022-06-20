1c3752ad217e8a5324a58ed17393ee8477d0ce7f
Guilherme Barbosa <g.mascarenhas@nofraud.com>
2022-06-19 21:23:38 -0300

    function approach

--- functionApproach.js
+++ functionApproach.js
@@ -9,6 +9,8 @@ fs.readFile(fileName, (err, data) => {
   dataHandler(data)
 })
 
+console.log('It will run first because the readFile is async and takes more time than console.log')
+
 // creating functions
 // How to run
 // $: node functionApproach.js

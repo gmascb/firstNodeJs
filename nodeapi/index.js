3d7caf9f4c1c9fe7c3329daf05407533aeb344f2
Guilherme Barbosa <g.mascarenhas@nofraud.com>
2022-06-19 22:11:39 -0300

    middleware

--- nodeapi/index.js
+++ nodeapi/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const port = 3001
 
+// middleware
+const morgan = require('morgan')
+
 // instantiate the express app
 const app = express()
 
@@ -8,8 +11,28 @@ const app = express()
 // const postRoutes = require('./routes/post')
 const { getPosts } = require('./routes/post')
 
+// morgan middleware
+// it will log every request on the console
+/*
+[nodemon] starting `node index.js`
+Nodejs listening on port 3001.
+GET / 304 3.085 ms - -
+*/
+app.use(morgan('dev'))
+
+// creating our own middleware
+const myOwnMiddleware = (req, res, next) => {
+  console.log('myOwnMiddleware applied.')
+  
+  // the next function needs to be called if it won't get stuck
+  next();
+}
+// the middleware can be added with the method .use() or adding it to the endpoint
+// app.use(myOwnMiddleware)
+
 // creating endpoint with the method getPosts
-app.get('/', getPosts)
+// adding the middleware into our endpoint
+app.get('/', myOwnMiddleware, getPosts)
 
 // starting server
 app.listen(port, () => { console.log(`Nodejs listening on port ${port}.`) })

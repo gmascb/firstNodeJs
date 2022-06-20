7e6507d1f8dcffc215a55b28b42ead598fb19da0
Guilherme Barbosa <g.mascarenhas@nofraud.com>
2022-06-19 22:57:39 -0300

    middleware

--- nodeapi/index.js
+++ nodeapi/index.js
@@ -7,9 +7,12 @@ const morgan = require('morgan')
 // instantiate the express app
 const app = express()
 
-// bring in routes
+// importing the method from routes/post | you can use directly or as middleware
 // const postRoutes = require('./routes/post')
-const { getPosts } = require('./routes/post')
+const { getPosts } = require('./routes/postRoutes.js')
+
+// using routes as middleware, the routes now send the responsability to the controller
+const postRoutes = require('./routes/postRoutes')
 
 // morgan middleware
 // it will log every request on the console
@@ -24,15 +27,19 @@ app.use(morgan('dev'))
 const myOwnMiddleware = (req, res, next) => {
   console.log('myOwnMiddleware applied.')
   
-  // the next function needs to be called if it won't get stuck
-  next();
+  next(); // the next function needs to be called if it won't get stuck
 }
 // the middleware can be added with the method .use() or adding it to the endpoint
 // app.use(myOwnMiddleware)
 
-// creating endpoint with the method getPosts
-// adding the middleware into our endpoint
-app.get('/', myOwnMiddleware, getPosts)
+// creating endpoint with the method getPosts and adding the middleware into our endpoint
+// console.log('getposts: ' + getPosts)
+// app.get('/myOwnMiddleware', myOwnMiddleware, getPosts)
+app.get('/myOwnMiddleware', myOwnMiddleware, getPosts)
+
+// using with routes as middleware
+app.use('/', postRoutes.getPosts)
+
 
 // starting server
 app.listen(port, () => { console.log(`Nodejs listening on port ${port}.`) })
---
+++ nodeapi/routes/postRoutes.js
@@ -0,0 +1,39 @@
+// this file will be responsible to link the routes and controllers
+
+// creating the router
+const express = require('express')
+const router = express.Router();
+
+// importing the controller
+const postController = require('../controllers/postController.js')
+
+
+// linking the path with the controller
+router.get('/', postController.getPosts)
+
+// exporting router -> use this when you want to export the entire module
+// module.exports = router;
+
+
+// use this to export several objects/functions
+exports.router = () => {
+  return router;
+}
+
+exports.getPosts = (req, res) => {
+  res.send('Hello getPosts');
+}
+
+// ==================================================================
+
+// const getPosts = (req, res) => {
+//   res.send('Hello getPosts');
+// }
+
+// module.exports = {
+//   getPosts
+// }
+
+// exports.getPosts = (req, res) => {
+//   res.send('Hello getPosts');
+// }
